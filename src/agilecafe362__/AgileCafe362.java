4748cc3e5e7d2decb99572dce5cc16b378a4c85b
Kevin <vuongkevin1@gmail.com>
2016-07-19 11:17:57 -0700

    Added init, stop functions and admin window/button

--- src/agilecafe362__/AgileCafe362.java
+++ src/agilecafe362__/AgileCafe362.java
@@ -6,46 +6,78 @@
 package agilecafe362__;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Aven
- */
 public class AgileCafe362 extends Application {
+    
+    Button AccessAdminButton;
+    BorderPane mainPane;
+    Scene mainScene;
+    
+    @Override
+    public void init() throws Exception {
+        //init() runs before application starts
+        //Connect to database
+        
+        //Query info from database into application
+        
+    }
+    
+    @Override
+    public void stop() throws Exception {
+        //stop() runs after application stops
+        //Save info into database
+        
+        //Close connection to database
+    }
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-
-                //System.out.println("Hisdsdasdasd");
-
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
+        mainPane = new BorderPane();
+        AccessAdminButton = new Button("Admin Login");
+        AccessAdminButton.setOnAction(e->buildLogInStage(primaryStage));
+        mainPane.setBottom(AccessAdminButton);
+        mainPane.setAlignment(AccessAdminButton, Pos.BOTTOM_RIGHT);
+        mainPane.setMargin(AccessAdminButton, new Insets(15,25,20,15));
+        mainScene = new Scene(mainPane, 700, 700);
         
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        primaryStage.setTitle("Main Menu");
+        primaryStage.setScene(mainScene);
         primaryStage.show();
     }
+    
+    private void buildLogInStage(Stage primaryStage){
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        Text title = new Text("Admin LogIn Page");
+        title.setFont(Font.font("Times New Roman", FontWeight.BOLD, 15));
+        grid.add(title,0,0,3,1);
+        
+        Label userName = new Label("Username: ");
+        grid.add(userName, 0, 1);
+        
+        Scene logInScene = new Scene(grid,500,500);
+        Stage logInStage = new Stage();
+        logInStage.setTitle("Admin Log in Page");
+        logInStage.setScene(logInScene);
+        primaryStage.close();
+        primaryStage.setOpacity(.5);
+        logInStage.show();
+        
+    }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
