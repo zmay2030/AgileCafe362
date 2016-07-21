06e3b717b57dd4b1ee34bf707aa55af8e18d0064
Binh Lam <raymondlam012892@gmail.com>
2016-07-21 12:54:52 -0700

    addOn

---
+++ src/agilecafe362__/Item.java
@@ -0,0 +1,106 @@
+package agilecafe362__;
+
+import java.text.NumberFormat;
+
+/**
+ *
+ * @author Dragon
+ */
+public class Item {
+    private double price;
+    private int itemID;
+    private String name;
+    private String description;
+    private int type;
+    private String image_path;
+    private Boolean deleted;
+    private int timesOrdered = 0;
+    
+    
+    
+    public Item(int itemID, String name, String description, int type, double price){
+        this.name = name;
+        this.itemID = itemID;
+        this.type = type;
+        this.description = description;
+        this.price = price;
+        deleted = false;
+    }
+    
+    public Item(int itemID, String name, String description, int type, double price, String image_path){
+        this.name = name;
+        this.itemID = itemID;
+        this.type = type;
+        this.description = description;
+        this.price = price;
+        this.image_path = image_path;
+    }
+    
+    public void incOrder(){
+        timesOrdered++;
+    }
+    
+    public int getTimesOrdered(){
+        return timesOrdered;
+    }
+    public int getItemID(){
+        return itemID;
+    }
+    
+    public void setItemID(int itemID){
+        this.itemID = itemID;
+    }
+    
+    public int getType(){
+        return type;
+    }
+    
+    public void setCategory(int category){
+        this.type = category;
+    }
+    
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public String getImage(){
+        return image_path;
+    }
+    public Boolean isDeleted()
+    {
+        return deleted;
+    }
+    public void SetDelete()
+    {
+        deleted = true;
+    }
+    public void setImage(String image_path){
+        this.image_path = image_path;
+    }
+    
+    
+    public String toString(){
+        NumberFormat fcurr = NumberFormat.getCurrencyInstance();
+        if(image_path == null)
+            return name + "(" + type + ")" +"\t" + description + "\t" + fcurr.format(price);
+        else
+            return name + "(" + type + ")" +"\t" + description + "\t" + fcurr.format(price) + "\t" + image_path;
+    }
+    
+    
+    
+}
